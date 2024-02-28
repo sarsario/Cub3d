8c1980382bb48a37cd82e9c380140f083005cf89
osarsari <o.sarsari@gmail.com>
2024-02-28 19:41:29 +0100

    some light was seen

--- src/utils/ray_cast.c
+++ src/utils/ray_cast.c
@@ -6,11 +6,26 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 17:04:11 by osarsari          #+#    #+#             */
-/*   Updated: 2024/02/28 18:47:22 by osarsari         ###   ########.fr       */
+/*   Updated: 2024/02/28 19:41:14 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+void	delta_calc(t_delta *delta, t_point *start, double angle)
+{
+	delta->dx = cos(angle);
+	delta->dy = sin(angle);
+	delta->sx = -1;
+	delta->sy = -1;
+	if (start->x < start->x + delta->dx)
+		delta->sx = 1;
+	if (start->y < start->y + delta->dy)
+		delta->sy = 1;
+	delta->err = -delta->dy / 2;
+	if (delta->dx > delta->dy)
+		delta->err = delta->dx / 2;
+}
 
 /*
 ** The purpose of the ray casting in this project is to determine the
@@ -19,3 +34,15 @@
 ** draw a line between them using the draw_line function.
 ** Concerns: draw_line draws one color. What about textures?
 */
+
+// Actually, same as draw_line, but while loop condition ends when we reach
+// a wall in the map instead of reaching the second point
+void	ray_cast(t_point *start, double angle, int **map)
+{
+	// From point p and angle, we can determine dx and dy
+	t_point	p;
+	t_delta	delta;
+	p.x = start->x;
+	p.y = start->y;
+	delta_calc(&delta, &p, angle);
+}
